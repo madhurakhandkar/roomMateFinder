d6f7a1805b1746008f0fa10014f6e116d7aac530
madhurakhandkar <madhu.khandkar@gmail.com>
2024-06-05 12:26:56 -0400

    sign up/login screen draft commit

--- app/src/main/java/com/example/roombuds/MainActivity.kt
+++ app/src/main/java/com/example/roombuds/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.roombuds
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,5 +18,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Start the LoginActivity
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
+        // Optional: Finish the MainActivity
+        finish()
+
+        val intent2 = Intent(this, SignInActivity::class.java)
+        startActivity(intent2)
+
+        finish()
     }
 }
